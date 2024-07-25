8bad6ec5d66a675effb347e38c51d4b1ede7e7c0
fdoooch <fdoooch@gmail.com>
2024-07-25 13:42:57 -0300

    extract sybol to the param of function produce_trades

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,37 +1,42 @@
-from pathlib import Path
-from pydantic_settings import BaseSettings
-from pydantic import BaseModel
-import os
-from dotenv import load_dotenv
 import logging
+import os
+from pathlib import Path
 
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DOTENV_PATH = os.path.join(BASE_DIR, '.env')
+DOTENV_PATH = os.path.join(BASE_DIR, ".env")
 load_dotenv(f"{DOTENV_PATH}")
 
+
 class KafkaSettings(BaseModel):
-    BROKER_ADDRESS: str = os.getenv('KAFKA_BROKER_ADDRESS', "localhost:19092")
-    TRADES_TOPIC: str = os.getenv('KAFKA_TRADES_TOPIC', "trade")
+	BROKER_ADDRESS: str = os.getenv("KAFKA_BROKER_ADDRESS", "localhost:19092")
+	TRADES_TOPIC: str = os.getenv("KAFKA_TRADES_TOPIC", "trade")
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Realtime Trade Producer"
-    PROJECT_VERSION: str = "0.0.1"
-    PROJECT_DESCRIPTION: str = "Produce trade stream to Redpanda"
-    BASE_DIR: Path = BASE_DIR
-    kafka: KafkaSettings = KafkaSettings()
-    LOGGER_NAME: str = "trade_producer"
+	PROJECT_NAME: str = "Realtime Trade Producer"
+	PROJECT_VERSION: str = "0.0.1"
+	PROJECT_DESCRIPTION: str = "Produce trade stream to Redpanda"
+	BASE_DIR: Path = BASE_DIR
+	kafka: KafkaSettings = KafkaSettings()
+	LOGGER_NAME: str = "trade_producer"
 
-settings = Settings()
 
-logging.basicConfig(level=logging.WARNING, format="%(asctime)s %(levelname).3s | %(name)s -> %(funcName)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-fmt = logging.Formatter(fmt="%(asctime)s %(levelname).3s | %(name)s -> %(funcName)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-logger = logging.getLogger(settings.LOGGER_NAME)
+config = Settings()
+
+logging.basicConfig(
+	level=logging.WARNING,
+	format="%(asctime)s %(levelname).3s | %(name)s -> %(funcName)s | %(message)s",
+	datefmt="%Y-%m-%d %H:%M:%S",
+)
+fmt = logging.Formatter(
+	fmt="%(asctime)s %(levelname).3s | %(name)s -> %(funcName)s | %(message)s",
+	datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(config.LOGGER_NAME)
 for handler in logger.handlers:
-    handler.setFormatter(fmt)
+	handler.setFormatter(fmt)
 logger.setLevel(logging.DEBUG)
-
-
-
-
--- services/trade_producer/src/main.py
+++ services/trade_producer/src/main.py
@@ -1,49 +1,43 @@
-from src.kraken_api import KrakenWebsocketTradeAPI
-from src.config import settings
+import logging
+import time
 
 from quixstreams import Application
-import time
-import logging
 
-logger = logging.getLogger(settings.LOGGER_NAME)
+from src.config import config
+from src.kraken_api import KrakenWebsocketTradeAPI
+
+logger = logging.getLogger(config.LOGGER_NAME)
 
 
-def produce_trades(
-        kafka_broker_address: str,
-        kafka_topic: str
-) -> None:
-    """
-    Reads trades from the Kraken websocket API and sends them to a Kafka topic
+def produce_trades(kafka_broker_address: str, kafka_topic: str, symbol: str) -> None:
+	"""
+	Reads trades from the Kraken websocket API and sends them to a Kafka topic
 
-    Args:
-        kafka_broker_address: The address of the Kafka broker
-        kafka_topic: The name of the Kafka topic
-    """
-    logger.debug(f"KAFKA BROKER ADDRESS: {kafka_broker_address}")
-    app = Application(kafka_broker_address)
+	Args:
+	    kafka_broker_address: The address of the Kafka broker
+	    kafka_topic: The name of the Kafka topic
+		symbol: The symbol to subscribe to trades for e.g. BTCUSDT
+	"""
+	logger.debug(f"KAFKA BROKER ADDRESS: {kafka_broker_address}")
+	app = Application(kafka_broker_address)
 
-    topic = app.topic(kafka_topic, value_serializer='json')
-    symbol = "BTCUSDT"
-    kraken_api = KrakenWebsocketTradeAPI(symbol)
+	topic = app.topic(kafka_topic, value_serializer="json")
+	kraken_api = KrakenWebsocketTradeAPI(symbol)
 
-    with app.get_producer() as producer:
-        while True:
-            trades = kraken_api.get_trades()
-            for trade in trades:
-                message = topic.serialize(key=trade["symbol"], value=trade)
+	with app.get_producer() as producer:
+		while True:
+			trades = kraken_api.get_trades()
+			for trade in trades:
+				message = topic.serialize(key=trade["symbol"], value=trade)
 
-                producer.produce(
-                    topic=topic.name, value=message.value, key=message.key
-                )
-                logger.debug(f"Produced message: {message}")
-                time.sleep(1)
-
+				producer.produce(topic=topic.name, value=message.value, key=message.key)
+				logger.debug(f"Produced message: {message}")
+				time.sleep(1)
 
 
 if __name__ == "__main__":
-    # print(f"Base directory: {settings.BASE_DIR}")
-    
-    produce_trades(
-        kafka_broker_address=settings.kafka.BROKER_ADDRESS,
-        kafka_topic=settings.kafka.TRADES_TOPIC
-    )
+	produce_trades(
+		kafka_broker_address=config.kafka.BROKER_ADDRESS,
+		kafka_topic=config.kafka.TRADES_TOPIC,
+        symbol="BTCUSDT",
+	)
