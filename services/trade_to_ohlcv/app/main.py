e3b11b5fc7ab68c237d7ca722e6c8a37ae702749
fdoooch <fdoooch@gmail.com>
2024-09-18 22:23:27 +0300

    install quixstreams

--- services/trade_to_ohlcv/app/main.py
+++ services/trade_to_ohlcv/app/main.py
@@ -1,6 +1,25 @@
-
+from quixstreams import Application
 
 async def trade_to_ohlcv(
     kafka_input_topic: str,
-    kafka_
-)
+    kafka_output_topic: str,
+    kafka_broker_address: str,
+    ohlcv_window_seconds: int,
+) -> None:
+    """
+    Reads trades from Kafka input_topic,
+    aggregates them into OHLCV with specified window size and 
+    writes OHLCV to Kafka output topic
+
+    Args:
+        kafka_input_topic: Kafka topic to read trades from
+        kafka_output_topic: Kafka topic to write OHLCV to
+        kafka_broker_address: Kafka broker address
+        ohlcv_window_seconds: Window size in seconds to aggregate trades into OHLCV
+
+    Returns:
+        None
+    """
+    app = Application(
+        broker_address=kafka_broker_address,
+    )
