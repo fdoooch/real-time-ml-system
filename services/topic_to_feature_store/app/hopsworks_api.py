1407da0e961430225600e613be511def5c990a62
fdoooch <fdoooch@gmail.com>
2024-09-24 23:11:30 -0300

    debugging hopsworks

--- services/topic_to_feature_store/app/hopsworks_api.py
+++ services/topic_to_feature_store/app/hopsworks_api.py
@@ -23,13 +23,11 @@ def get_or_create_feature_group(
     options: FeatureGroupOptions,
     creds: FeatureGroupCreds,
 ) -> FeatureGroup:
-    conn = hsfs.connection(
-        host="c.app.hopsworks.ai",
-        project=creds.project_name,
+    project = hopsworks.login(
+        project=creds.project_name, 
         api_key_value=creds.api_key,
     )
-    
-    feature_store = conn.get_feature_store()
+    feature_store = project.get_feature_store()
 
     return feature_store.get_or_create_feature_group(
         name=options.name,
@@ -61,37 +59,37 @@ def push_feature_to_feature_group(
     print("✔️ Done")
 
 
-def push_feature_to_feature_store(
-        options: FeatureGroupOptions,
-        creds: FeatureGroupCreds,
-        feature: dict,
-        start_offline_materialization: bool,
-) -> None:
-    project = hopsworks.login(
-        project=creds.project_name, 
-        api_key_value=creds.api_key,
-    )
-    feature_store = project.get_feature_store()
+# def push_feature_to_feature_store(
+#         options: FeatureGroupOptions,
+#         creds: FeatureGroupCreds,
+#         feature: dict,
+#         start_offline_materialization: bool,
+# ) -> None:
+#     project = hopsworks.login(
+#         project=creds.project_name, 
+#         api_key_value=creds.api_key,
+#     )
+#     feature_store = project.get_feature_store()
 
-    feature_group = feature_store.get_or_create_feature_group(
-        name=options.name,
-        version=options.version,
-        primary_key=options.primary_key,
-        online_enabled=options.online_enabled,
-        # expectation_suite=...
-    )
+#     feature_group = feature_store.get_or_create_feature_group(
+#         name=options.name,
+#         version=options.version,
+#         primary_key=options.primary_key,
+#         online_enabled=options.online_enabled,
+#         # expectation_suite=...
+#     )
 
-    if isinstance(feature, dict):
-        if all(isinstance(v, (list, tuple)) for v in feature.values()):
-            feature_df = pd.DataFrame(feature)
-        else:
-            feature_df = pd.DataFrame([feature])
-    else:
-        raise ValueError("Feature must be a dictionary")
-    feature_group.insert(
-        feature_df,
-        # write_options={
-        #     "start_offline_materialization": start_offline_materialization,
-        # },
-    )
-    print("✔️ Done")
+#     if isinstance(feature, dict):
+#         if all(isinstance(v, (list, tuple)) for v in feature.values()):
+#             feature_df = pd.DataFrame(feature)
+#         else:
+#             feature_df = pd.DataFrame([feature])
+#     else:
+#         raise ValueError("Feature must be a dictionary")
+#     feature_group.insert(
+#         feature_df,
+#         # write_options={
+#         #     "start_offline_materialization": start_offline_materialization,
+#         # },
+#     )
+#     print("✔️ Done")
