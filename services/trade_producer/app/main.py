8923e699ec3be1f7dc09f5fc853cc583dc710107
fdoooch <fdoooch@gmail.com>
2024-10-08 22:19:00 -0300

    fix kafka topic name for trade producer

--- services/trade_producer/app/main.py
+++ services/trade_producer/app/main.py
@@ -76,13 +76,13 @@ class TradesProducer:
 
 def get_trades_connector() -> TradesConnector:
 	if settings.trades_source.NAME == TradeSourceName.KRAKEN_SPOT:
-		settings.kafka.TRADES_TOPIC = "trades_kraken_spot"
+		# settings.kafka.TRADES_TOPIC = "trades_kraken_spot"
 		return KrakenTradesConnector()
 	elif settings.trades_source.NAME == TradeSourceName.KRAKEN_SPOT_HISTORICAL:
-		settings.kafka.TRADES_TOPIC = "trades_kraken_spot_historical"
+		# settings.kafka.TRADES_TOPIC = "trades_kraken_spot_historical"
 		return KrakenHistoricalTradesConnector()
 	elif settings.trades_source.NAME == TradeSourceName.BYBIT_SPOT:
-		settings.kafka.TRADES_TOPIC = "trades_bybit_spot"
+		# settings.kafka.TRADES_TOPIC = "trades_bybit_spot"
 		return BybitSpotTradesConnector()
 	raise NotImplementedError
 
